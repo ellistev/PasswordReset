00fabb1b8bea68bd824643f2575f780a0b26ca89
Steven Elliott <steven.peter.elliott@gmail.com>
2016-06-22 21:33:54 -0700

    will now send message after unlocking

--- PasswordResetWeb/Controllers/UnlockAccountController.cs
+++ PasswordResetWeb/Controllers/UnlockAccountController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using Twilio;
 
 namespace PasswordResetWeb.Controllers
 {
@@ -15,11 +16,28 @@ namespace PasswordResetWeb.Controllers
         public ActionResult Reset()
         {
 
-            var proc = Process.Start("../../PasswordReset/bin/debug/PollGeographicCoordinates.exe");
+            var proc = Process.Start("c:/reset/PollGeographicCoordinates.exe");
             proc.WaitForExit();
             var exitCode = proc.ExitCode;
 
-            return View("index");
+            string AccountSid = "ACe438f581edceafffdb27daacc6da1b87";
+            string AuthToken = "412cd77649e095da11c2b0ff6ac2305b";
+
+            // instantiate a new Twilio Rest Client
+            var client = new TwilioRestClient(AccountSid, AuthToken);
+
+
+            // Send a new outgoing SMS by POSTing to the Messages resource */
+            client.SendMessage(
+                "587-600-0677", // From number, must be an SMS-enabled Twilio number
+                "+14039217113",     // To number, if using Sandbox see note above
+                                    // message content
+                string.Format("Hey Steve, Account Unlocked!")
+            );
+
+            Response.Write(string.Format("Sent message to Steve"));
+
+            return View("Reset");
         }
     }
 }
